e570f2b0849e855349744561ab6bd29d3822df51
lyp1noff <lyp1noff@gmail.com>
2023-11-14 18:46:10 +0000

    build.sh update

--- MacOS/build.sh
+++ MacOS/build.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 APP_NAME="./Champion.app"
-RUNTIME="osx-arm64"
+RUNTIME="osx-$1"
 PUBLISH_OUTPUT_DIRECTORY="../Champion.Desktop/bin/publish/$RUNTIME/"
 # PUBLISH_OUTPUT_DIRECTORY should point to the output directory of your dotnet publish command.
 # One example is /path/to/your/csproj/bin/Release/netcoreapp3.1/osx-x64/publish/.
 # If you want to change output directories, add `--output /my/directory/path` to your `dotnet publish` command.
 INFO_PLIST="./Info.plist"
-ICON_FILE="./../Assets/Icon.icns"
+ICON_FILE="../Assets/Icon.icns"
 
 if [ -d "$APP_NAME" ]
 then
@@ -15,7 +15,6 @@ then
 fi
 
 cd ../Champion.Desktop
-dotnet restore
 dotnet publish -r $RUNTIME --self-contained --configuration Release -p:UseAppHost=true --output ./bin/publish/$RUNTIME
 cd ../MacOS
 
@@ -26,7 +25,7 @@ mkdir "$APP_NAME/Contents/MacOS"
 mkdir "$APP_NAME/Contents/Resources"
 
 cp "$INFO_PLIST" "$APP_NAME/Contents/Info.plist"
-cp "$ICON_FILE" "$APP_NAME/Contents/Resources/$ICON_FILE"
+cp "$ICON_FILE" "$APP_NAME/Contents/Resources/Icon.icns"
 
 chmod +x "$PUBLISH_OUTPUT_DIRECTORY/Champion.Desktop"
 cp -a "$PUBLISH_OUTPUT_DIRECTORY" "$APP_NAME/Contents/MacOS"
