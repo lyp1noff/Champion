044910c0c9e094c3005f7e95f895d0e42f3284ce
lyp1noff <lyp1noff@gmail.com>
2024-01-24 20:28:03 +0000

    Dynamic BracketEditor

--- Champion/Views/BracketEditorView.axaml.cs
+++ Champion/Views/BracketEditorView.axaml.cs
@@ -1,4 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Media;
+
 namespace Champion.Views;
 
 public partial class BracketEditorView : UserControl
@@ -6,5 +12,99 @@ public partial class BracketEditorView : UserControl
     public BracketEditorView()
     {
         InitializeComponent();
+    }
+    
+    public void ShowBracket(IEnumerable<Competitor> bracket)
+    {
+        BracketGrid.Children.Clear();
+
+        var splitList = Utils.SplitCompetitors(bracket.Count(), App.AppConfig.MaxCompetitorsPerGroup);
+        var splitTypeIsRound = (bool)radioButtonLimitRound.IsChecked!;
+        if (splitTypeIsRound) 
+            splitList = Utils.SplitCompetitors(bracket.Count(), App.AppConfig.MaxCompetitorsPerRoundGroup);
+
+        var index = 0;
+        var rowIndex = 0;
+        foreach (var competitor in bracket)
+        {
+            var textBlock = new TextBlock();
+            textBlock.Text = $"{competitor.SortId}. " + competitor.GetFullName();
+            textBlock.Tag = competitor;
+            // textBlock.AllowDrop = true;
+            // textBlock.Drop += TextBlock_Drop;
+            // textBlock.MouseMove += TextBlock_MouseMove;
+
+            var contextMenu = new ContextMenu();
+
+            var editMenuItem = new MenuItem();
+            editMenuItem.Header = "Изменить";
+            // editMenuItem.Click += OpenEditCompetitorWindow;
+            contextMenu.Items.Add(editMenuItem);
+
+            var deleteMenuItem = new MenuItem();
+            deleteMenuItem.Header = "Удалить";
+            // deleteMenuItem.Click += DeleteCompetitor;
+            contextMenu.Items.Add(deleteMenuItem);
+
+            textBlock.ContextMenu = contextMenu;
+
+            textBlock.Padding = new Thickness(5);
+            textBlock.Width = 350;
+            // textBlock.VerticalAlignment = VerticalAlignment.Center;
+
+            textBlock.DataContext = competitor;
+            // textBlock.PreviewMouseRightButtonDown += TextBlock_PreviewMouseRightButtonDown;
+
+            if (splitList[0] == index && index > 1)
+            {
+                splitList.RemoveAt(0);
+                index = 0;
+                var separator = new Separator();
+                separator.Height = 5;
+                separator.Width = 350;
+                separator.Margin = new Thickness(10, 10, 10, 0);
+                separator.Background = new SolidColorBrush(Colors.White);
+                Grid.SetRow(separator, rowIndex);
+                BracketGrid.Children.Add(separator);
+                rowIndex++;
+            }
+
+            if (splitTypeIsRound)
+            {
+                textBlock.Margin = new Thickness(10, 10, 10, 0);
+                textBlock.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0x57, 0x4E, 0xB9));
+            }
+            else
+            {
+                textBlock.Margin = new Thickness(10, 10, 10, 0);
+                if (index % 2 == 1)
+                {
+                    textBlock.Background = new SolidColorBrush(Colors.DodgerBlue);
+                }
+                else
+                {
+                    textBlock.Background = new SolidColorBrush(Colors.DarkRed);
+                    if (index > 1)
+                        textBlock.Margin = new Thickness(10, 20, 10, 0);
+                }
+            }
+
+            var row = new RowDefinition();
+            row.Height = new GridLength(1, GridUnitType.Auto);
+            BracketGrid.RowDefinitions.Add(row);
+
+            Grid.SetRow(textBlock, rowIndex);
+            BracketGrid.Children.Add(textBlock);
+            rowIndex++;
+
+            index++;
+        }
+    }
+
+    private void CategoriesListBox_OnSelectionChanged(object? sender, SelectionChangedEventArgs e)
+    {
+        var data = (string)CategoriesListBox.SelectedItem!;
+        if (!String.IsNullOrEmpty(data))
+            ShowBracket(App.CompetitorManager.GetBracket(data));
     }
 }
