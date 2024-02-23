dd1ba11b9a9b05a12854a7593d3c004c2b59ea01
lyp1noff <lyp1noff@gmail.com>
2024-02-23 03:50:41 +0000

    upd to master branch

--- MacOS/install.sh
+++ MacOS/install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# curl -s https://git.noboobs.help/lyp1noff/Champion/raw/branch/master/MacOS/install.sh | bash -s arm64/x64
+
 TEMPDIR="$TMPDIR/champion"
 APP="./Champion.app"
 RUNTIME="osx-$1"
@@ -18,9 +20,9 @@ mkdir "$APP/Contents"
 mkdir "$APP/Contents/MacOS"
 mkdir "$APP/Contents/Resources"
 
-curl https://git.noboobs.help/lyp1noff/Champion/raw/branch/beta/MacOS/Info.plist --output "$APP/Contents/Info.plist"
-curl https://git.noboobs.help/lyp1noff/Champion/raw/branch/beta/Assets/Champion.icns --output "$APP/Contents/Resources/Champion.icns"
-curl https://git.noboobs.help/lyp1noff/Champion/raw/branch/beta/Assets/ChampionBracket.icns --output "$APP/Contents/Resources/ChampionBracket.icns"
+curl https://git.noboobs.help/lyp1noff/Champion/raw/branch/master/MacOS/Info.plist --output "$APP/Contents/Info.plist"
+curl https://git.noboobs.help/lyp1noff/Champion/raw/branch/master/Assets/Champion.icns --output "$APP/Contents/Resources/Champion.icns"
+curl https://git.noboobs.help/lyp1noff/Champion/raw/branch/master/Assets/ChampionBracket.icns --output "$APP/Contents/Resources/ChampionBracket.icns"
 
 chmod +x "./$RUNTIME/Champion.Desktop"
 cp -a "./$RUNTIME/" "$APP/Contents/MacOS"
